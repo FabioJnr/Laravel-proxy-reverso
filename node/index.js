956cceee6cbe5e7dc468e0bc9fd8ac45c43e3f25
Fabio Silva <fbiojnior@gmail.com>
2021-12-27 16:54:57 -0300

    Trabalhando com node e MySQL

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const port = "3000";
 const app = express();
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Fabio')`;
+connection.query(sql);
+connection.end();
 
 app.get("/", (req, res) => {
   res.send("<h1>Server online</h1>");
